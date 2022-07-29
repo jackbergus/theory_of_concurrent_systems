4a103631349f7a9ef5a04123b72c6f63546d5169
Giacomo Bergami <ngb113@newcastle.ac.uk>
2022-07-29 23:13:27 +0100

    Removed unnecessary code from hml, and copyright added

--- include/ccs/generate_lts.h
+++ include/ccs/generate_lts.h
@@ -1,6 +1,22 @@
-//
-// Created by giacomo on 29/07/22.
-//
+/*
+ * generate_lts.h
+ * This file is part of hml
+ *
+ * Copyright (C) 2022 - Giacomo Bergami
+ *
+ * hml is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * hml is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with hml. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef CCS_GENERATE_LTS_H
 #define CCS_GENERATE_LTS_H
--- main_hml.cpp
+++ main_hml.cpp
@@ -18,50 +18,8 @@
  * along with hml. If not, see <http://www.gnu.org/licenses/>.
  */
 
- 
-
 #include <hml/hml.h>
 #include <iostream>
-
-enum ltlf_type {
-    ltlf_AND,
-    ltlf_OR,
-    ltlf_UNTIL,
-    ltlf_RELEASE,
-    ltlf_BOX,
-    ltlf_DIAM,
-    ltlf_ACT
-};
-
-struct ltlf_formula {
-    ltlf_type casusu;
-    std::vector<ltlf_formula> args;
-    std::string label;
-
-    bool operator==(const ltlf_formula &rhs) const {
-        return casusu == rhs.casusu &&
-               args == rhs.args &&
-               label == rhs.label;
-    }
-
-    bool operator!=(const ltlf_formula &rhs) const {
-        return !(rhs == *this);
-    }
-};
-
-namespace std {
-    template <> struct hash<ltlf_formula> {
-        const std::hash<std::string> h;
-        size_t operator()(const ltlf_formula& x) const {
-            size_t arg = ((size_t)x.casusu) ^ h(x.label) ^ x.args.size();
-            size_t i = 31;
-            for (const auto& ref : x.args)
-                i = (*this)(ref) * 7 + 31;
-            return arg * i;
-        }
-    };
-}
-
 
 int main() {
     LTS mlts1({Rel (0,"t",1), Rel (1,"t",0), Rel(2,"a",1), Rel (2,"a",3)});
