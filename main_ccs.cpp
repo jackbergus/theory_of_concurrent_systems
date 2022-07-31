194a51b8d6b1d9592c842749495b4305c21545b4
Giacomo Bergami <ngb113@newcastle.ac.uk>
2022-07-31 17:43:58 +0100

    Commented example

--- main_ccs.cpp
+++ main_ccs.cpp
@@ -33,6 +33,8 @@ int main() {
     act a_signed{"a", true};
     act b_signed{"b", true};
     act g_signed{"g", true};
+
+    // Actions that are propagated at the upper levels
     act a_out{"a_out"};
     act b_out{"b_out"};
     act g_out{"g_out"};
@@ -40,64 +42,50 @@ int main() {
     act b_out_signed{"b_out", true};
     act g_out_signed{"g_out", true};
 
-//    auto die = ccs::pref(a, ccs::nil());
 
-    auto attack_state_only = ccs::pref(b,  nullptr);
+    auto attack_state_only = ccs::pref(b,  nullptr); // nullptr: waiting for the recursive operator, not yet defined
     auto attack_state = ccs::cons(act("attack"), attack_state_only);
-    auto prepare_state_only = ccs::pref(b_signed,  nullptr);
+    auto prepare_state_only = ccs::pref(b_signed,  nullptr); // nullptr: waiting for the recursive operator, not yet defined
     auto prepare_state = ccs::cons(act("prepare"), prepare_state_only);
-    attack_state_only->set_recursive(prepare_state);
-    prepare_state_only->set_recursive(attack_state);
+    attack_state_only->set_recursive(prepare_state); // attack := b.prepare
+    prepare_state_only->set_recursive(attack_state); // prepare := b'.attack
 
     auto move_state_only = ccs::pref(g_signed,  nullptr);
     auto move_state = ccs::cons(act("move"), move_state_only);
     auto stand_state_only = ccs::pref(g,  nullptr);
     auto stand_state = ccs::cons(act("stand"), stand_state_only);
-    stand_state_only->set_recursive(move_state);
-    move_state_only->set_recursive(stand_state);
+    stand_state_only->set_recursive(move_state); // stand := g.move
+    move_state_only->set_recursive(stand_state); // move := g'.stand
 
-    auto move_up_state = ccs::pref(pop, nullptr);
-    auto g_stay = ccs::pref(g, ccs::pref(g_out, nullptr));
-    auto b_stay = ccs::pref(b, ccs::pref(b_out, nullptr));
+
+    auto move_up_state = ccs::pref(pop, nullptr); // moving to tup to the upper hierarchy
+    auto g_stay = ccs::pref(g, ccs::pref(g_out, nullptr)); // the internal event is capturing the sole g event, and propagating g on the upper hierarchy
+    auto b_stay = ccs::pref(b, ccs::pref(b_out, nullptr)); // same as above...
     auto gs_stay = ccs::pref(g_signed, ccs::pref(g_out_signed, nullptr));
     auto bs_stay = ccs::pref(b_signed, ccs::pref(b_out_signed, nullptr));
-    auto actual_sync = ccs::cons(act("loop"), ccs::sum({g_stay, gs_stay, b_stay, bs_stay, move_up_state}));
+
+    // loop := g.g_out.loop + b.b_out.loop + g'.g_out'.loop + b'.b_out'.loop + move_up_state
+    auto actual_sync = ccs::cons(act("loop"), ccs::sum({g_stay, gs_stay, b_stay, bs_stay, move_up_state})); // System syncronization at the nested level
+
+    // sync := push'.loop
     auto sync = ccs::cons(act("sync"), ccs::pref(push_w, actual_sync));
     g_stay->set_recursive(actual_sync);
     gs_stay->set_recursive(actual_sync);
     b_stay->set_recursive(actual_sync);
     bs_stay->set_recursive(actual_sync);
-    move_up_state->set_recursive(sync);
+    move_up_state->set_recursive(sync); // move_up_state := pop.synch
 
+    // alive := (attack | move | sync)
     auto alive_state = ccs::par({attack_state, move_state, sync});
+    // synch_alive := (v g,b) alive
     auto sync_alive = ccs::restr(alive_state, {g, b});
 
-
-//
-//
-//    auto alive = ccs::cons(act("alive"), alive_state);
-//
-//    auto pasive_left_sum = ccs::par({attack_state, stand_state});
-//    auto pasive_sum = ccs::sum({pasive_left_sum, alive});
-//    auto passive = ccs::cons(act("passive"), pasive_sum);
-//    alive_state->set_recursive(passive);
-
-
+    // die := a'.push.pop'.a.die
     auto die = ccs::cons(act("die"), ccs::pref(a_signed, ccs::pref(push, ccs::pref(pop_w, ccs::pref(a, nullptr)))));
     die->set_recursive(die);
 
-
+    // system := (v push,pop)(die|alive)
     auto system = ccs::restr(ccs::par({sync_alive, die}), {push, pop});
-
-//    auto dead_state_only = ccs::pref(a_signed,  nullptr);
-//    auto dead_state = ccs::cons(act("dead"), dead_state_only);
-//    auto a_sum_right = ccs::pref(a,  nullptr);
-//    auto a_sum = ccs::sum({a_sum_right, pasive});
-//    a_sum_right->set_recursive(dead_state);
-//    auto alive_state_sum = ccs::sum({pasive, a_sum_right});
-//    auto alive_state = ccs::cons(act("alive"), alive_state_sum);
-//    dead_state_only->set_recursive(alive_state);
-
 
     // Printing the graph
     for (const auto& x : build_graph(*system)) {
